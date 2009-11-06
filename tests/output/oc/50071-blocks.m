d54b9f581a97fcd516a51252d7a3aa388b00aa62
André Berg <andre.bergmedia@googlemail.com>
2009-11-06 11:40:57 +0100

    Update outputs for tests dealing with ObjC blocks.

--- tests/output/oc/50071-blocks.m
+++ tests/output/oc/50071-blocks.m
@@ -1,14 +1,14 @@
 int* ( ^  blkReturningPointer)(int) = ^ int* (int a) {
-   return a + 1;
+    return a + 1;
 };
 
 void ( ^ blk2)(int *) = ^(int *b) {
-   *b = 1;
+    *b = 1;
 };
 
 
 int ( ^oneFrom)(int) = ^  (int anInt) {
-   return anInt - 1;
+    return anInt - 1;
 };
 
 // this should not be flagged as OC_BLOCK_CARET
